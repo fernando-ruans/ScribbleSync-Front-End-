e3710bed2e5c39027dc6231ba2ea039f27711ca2
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-01 11:42:47 -0300

    Aplicando estilo global.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
-
+import GlobalStyles from './styles/global';
 import theme from "./styles/theme";
 
 import { Details } from "./pages/Details";
@@ -9,6 +9,7 @@ import { Details } from "./pages/Details";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </React.StrictMode>
