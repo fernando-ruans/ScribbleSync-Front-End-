b96621b859d39da49122708bd984fd17a3d07aeb
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-02 09:51:28 -0300

    Utilizando children.

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,22 @@
-import { Container } from './styles';
+import { Container, Links } from './styles';
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
 
 export function Details() {
   return (
     <Container>
       <Header />
-      
-      <Button title="Voltar"/>
+
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://google.com</a></li>
+          <li><a href="#">https://google.com</a></li>   
+        </Links>
+      </Section>
+
+      <Button title="Voltar" />
     </Container>
   )
 }
