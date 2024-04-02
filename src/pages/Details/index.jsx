d4db83caa834f8b5a9df558c04eb67c6252ff171
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-02 10:30:00 -0300

    Componente tag.

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from './styles';
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag"
 
 export function Details() {
   return (
@@ -11,9 +12,18 @@ export function Details() {
 
       <Section title="Links úteis">
         <Links>
-          <li><a href="#">https://google.com</a></li>
-          <li><a href="#">https://google.com</a></li>   
+          <li>
+            <a href="#">https://google.com</a>
+          </li>
+          <li>
+            <a href="#">https://google.com</a>
+          </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
       </Section>
 
       <Button title="Voltar" />
