a3b27f56e84e01d50455f80ab93d8bbb1796fffe
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-02 15:21:55 -0300

    Logotipo.

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -16,27 +16,41 @@ export const Container = styled.div `
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
 `;
 
-export const Brand = styled.div `
+export const Brand = styled.div`
   grid-area: brand;
-  background: red;
-`;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  > h1 {
+    font-size: 24px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+`
 
 export const Menu = styled.ul`
   grid-area: menu;
-  background: green;
+  
 `;
 
 export const Search = styled.div`
   grid-area: search;
-  background: violet;
+  
 `;
 
 export const Content = styled.div`
   grid-area: content;
-  background: blue;
+  
 `;
 
 export const NewNote = styled.button`
   grid-area: newnote;
-  background: yellow;
+  
 `;
