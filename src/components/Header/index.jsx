8b684a3e1f3af274c587d8e227eb4f4e3915c93d
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-02 08:39:52 -0300

    Aplicando react icons.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,6 @@
-import { Container, Profile } from './styles';
+import { RiShutDownLine } from 'react-icons/ri';
+
+import { Container, Profile, Logout } from './styles';
 
 export function Header() {
   return (
@@ -14,6 +16,10 @@ export function Header() {
           <strong>Fernando Ruan</strong>
         </div>
       </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -42,4 +42,14 @@ export const Profile = styled.div`
       color: ${({ theme }) => theme.COLORS.WHITE};
     }
   }
-`
+`;
+
+export const Logout = styled.button`
+  border: none;
+  background: none;
+
+  > svg {
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+    font-size: 36px;
+  }
+`;
