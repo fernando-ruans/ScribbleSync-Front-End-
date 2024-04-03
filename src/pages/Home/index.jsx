972cb2714b6903fe4416dfdc61b7954122336a8e
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-03 12:08:29 -0300

    Componente de notas.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,9 @@ import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/Header';
+import { Note } from '../../components/Note';
 import { Input } from '../../components/input';
+import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
@@ -26,7 +28,17 @@ export function Home() {
       </Search>
 
       <Content>
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React',
+            tags: [
+              { id: '1', name: 'react' },
+              { id: '2', name: 'scribblesync' }
+            ]
+          }}
+          />
 
+        </Section>
       </Content>
 
       <NewNote>
