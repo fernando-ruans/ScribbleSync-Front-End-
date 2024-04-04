1ed4e0abfb7e09f7e20f2e98f50aaecc601c5f65
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-03 22:42:52 -0300

    Utilizando Imagens.

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -3,7 +3,7 @@ import { FiMail, FiLock } from "react-icons/fi";
 import { Input } from '../../components/Input';
 import { Button } from "../../components/Button";
 
-import { Container, Form } from "./styles"
+import { Container, Form, Background } from "./styles"
 
 export function SignIn() {
   return (
@@ -33,6 +33,8 @@ export function SignIn() {
         </a>         
         
       </Form>
+
+      <Background />
     </Container>
   );
 }
