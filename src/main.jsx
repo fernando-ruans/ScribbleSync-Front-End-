bc2352b7e78006fc9f3d2a114c931de3db869f1d
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-03 23:14:25 -0300

    SignUp.

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './styles/global';
 
 import theme from "./styles/theme";
 
-import { SignIn } from "./pages/Signin";
+import { SignUp } from "./pages/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 )
