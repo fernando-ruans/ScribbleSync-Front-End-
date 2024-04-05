148d042bfd9ed671a0f553911d8ab7ab3ae2c278
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-05 12:08:42 -0300

    Avatar do usuário.

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,9 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
   return (
@@ -15,26 +15,40 @@ export function Profile() {
       </header>
 
       <Form>
+      <Avatar>
+        <img src="https://github.com/fernando-ruans.png" 
+        alt="Foto de usuário" 
+        />
+
+        <label htmlFor="avatar">
+          <FiCamera />
+
+          <input 
+            id="avatar"
+            type="file"
+          />
+        </label>
+      </Avatar>  
         <Input
-          plceholder="Nome"
+          placeholder="Nome"
           type="text"
           icon={FiUser}
         />
 
         <Input
-          plceholder="E-mail"
+          placeholder="E-mail"
           type="text"
           icon={FiMail}
         />
 
         <Input
-          plceholder="Senha atual"
+          placeholder="Senha atual"
           type="password"
           icon={FiLock}
         />
 
         <Input
-          plceholder="Nova senha"
+          placeholder="Nova senha"
           type="password"
           icon={FiLock}
         />
