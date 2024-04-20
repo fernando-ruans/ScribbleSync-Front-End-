05bde60a77242f4a788c5f915d982b19a538bed7
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-20 18:02:26 -0300

    Criando contexto.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from './styles/global';
+import { MyContext } from "./myContext";
 
 import { Routes } from './routes';
 
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <MyContext.Provider value={ { name: 'fernando', email: 'fernando@email.com' }}>
+        <Routes />
+      </MyContext.Provider>
     </ThemeProvider>
   </React.StrictMode>
 )
