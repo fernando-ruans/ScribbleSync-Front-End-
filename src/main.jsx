7985946ad6578048ad053522be91f723218bad5a
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-21 12:47:04 -0300

    Hook de Autentificação.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from './styles/global';
-import { MyContext } from "./myContext";
+
+import { AuthProvider } from "./hooks/auth";
 
 import { Routes } from './routes';
 
@@ -12,9 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <MyContext.Provider value={ { name: 'fernando', email: 'fernando@email.com' }}>
+      <AuthProvider>
         <Routes />
-      </MyContext.Provider>
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { SignIn } from '../pages/Signin';
+import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp'
 
 
