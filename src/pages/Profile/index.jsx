a8c8e54cad851d621e5e47dec0a2e058ae88250b
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-23 13:35:13 -0300

    Carregando dados do usuário.

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 import { Link } from "react-router-dom";
+
+import { useAuth } from "../../hooks/auth";
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -7,6 +10,13 @@ import { Button } from '../../components/Button';
 import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
+  const { user } = useAuth();
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, setPasswordNew] = useState();
+  
   return (
     <Container>
       <header>
@@ -16,46 +26,50 @@ export function Profile() {
       </header>
 
       <Form>
-      <Avatar>
-        <img src="https://github.com/fernando-ruans.png" 
-        alt="Foto de usuário" 
-        />
-
-        <label htmlFor="avatar">
-          <FiCamera />
-
-          <input 
-            id="avatar"
-            type="file"
+        <Avatar>
+          <img
+            src="https://github.com/fernando-ruans.png"
+            alt="Foto de usuário"
           />
-        </label>
-      </Avatar>  
+
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
         <Input
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha atual"
           type="password"
           icon={FiLock}
+          onChange={(e) => setPasswordOld(e.target.value)}
         />
 
         <Input
           placeholder="Nova senha"
           type="password"
           icon={FiLock}
+          onChange={(e) => setPasswordNew(e.target.value)}
         />
 
-        <Button title="Salvar"/>
+        <Button title="Salvar" />
       </Form>
-    </ Container>
-  );
+    </Container>
+  )
 }
