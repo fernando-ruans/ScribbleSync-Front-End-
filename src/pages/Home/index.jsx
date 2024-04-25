2942a0022f3965994e513ba8c5734ed6775639ca
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-25 19:56:30 -0300

    Carregando tags.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from 'react';
 import { FiPlus, FiSearch } from 'react-icons/fi';
+
+import { api } from '../../services/api';
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/Header';
@@ -8,6 +12,18 @@ import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
+  const [tags, setTags] = useState([]);
+
+  
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags");
+      setTags(response.data)
+    }
+
+    fetchTags();
+  }, []);
+
   return (
     <Container>
       <Brand>
@@ -17,27 +33,35 @@ export function Home() {
       <Header />
 
       <Menu>
-        <li><ButtonText title="Todos" isActive /></li>
-        <li><ButtonText title="React" /></li>
-        <li><ButtonText title="NodeJs" /></li>
-
+        <li>
+          <ButtonText title="Todos" isActive />
+        </li>
+        {tags &&
+          tags.map((tag) => (
+            <li key={String(tag.id)}>
+              <ButtonText 
+              title={tag.name}  
+              />
+            </li>
+          ))
+        }
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título:" icon={FiSearch}/>
+        <Input placeholder="Pesquisar pelo título:" icon={FiSearch} />
       </Search>
 
       <Content>
         <Section title="Minhas notas">
-          <Note data={{
-            title: 'React',
-            tags: [
-              { id: '1', name: 'react' },
-              { id: '2', name: 'scribblesync' }
-            ]
-          }}
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: "1", name: "react" },
+                { id: "2", name: "scribblesync" },
+              ],
+            }}
           />
-
         </Section>
       </Content>
 
