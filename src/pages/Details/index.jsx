eed32f9cfad86ddf33492a4421a3fd744f025e25
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-26 12:25:23 -0300

    Remover notas.

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -21,6 +21,15 @@ export function Details() {
     navigate("/");
   }
 
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja remover a nota?");
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`);
+      navigate("/");
+    }
+  }
+
   useEffect(() => {
     async function fetchNote() {
       const response = await api.get(`/notes/${params.id}`);
@@ -37,7 +46,10 @@ export function Details() {
       {data && (
         <main>
           <Content>
-            <ButtonText title="Excluir nota" />
+            <ButtonText 
+              title="Excluir nota" 
+              onClick={handleRemove}
+            />
 
             <h1>{data.title}</h1>
 
