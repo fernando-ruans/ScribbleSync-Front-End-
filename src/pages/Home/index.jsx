b6e31997c62016a54baf8c4f49bce22971ef0475
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-26 10:42:59 -0300

    Abrir nota cadastrada.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { FiPlus, FiSearch } from 'react-icons/fi';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 import { api } from '../../services/api';
 
@@ -15,7 +16,9 @@ export function Home() {
   const [search, setSearch] = useState([])
   const [tags, setTags] = useState([]);
   const [tagsSelected, setTagsSelected] = useState([]);
-  const [notes, setNotes] = useState([]); 
+  const [notes, setNotes] = useState([]);
+
+  const navigate = useNavigate();
 
   function handleTagSelected(tagName) {
     if (tagName === "all") {
@@ -31,6 +34,10 @@ export function Home() {
     } else {
       setTagsSelected(prevState => [...prevState, tagName]);
     }    
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`);
   }
   
   useEffect(() => {
@@ -94,6 +101,7 @@ export function Home() {
             <Note
             key={String(note.id)}
             data={note}
+            onClick={() => handleDetails(note.id)}
             />
           ))
         }
